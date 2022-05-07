04a154b823bfa4e5b88fc5cc3a05393a1d92c403
kdcr <kevincabrerarodriguez@gmail.com>
2022-05-07 15:39:53 +0100

    Landing page development - Added button and translation

--- src/components/layout/landing/Landing.jsx
+++ src/components/layout/landing/Landing.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
+import { useNavigate } from 'react-router-dom';
 import Label from '../../atomic/label';
+import Button from '../../atomic/button';
 
 const LandingContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 2rem;
 
   width: 100%;
   height: 100%;
@@ -16,9 +20,12 @@ const LandingContainer = styled.div`
 const Landing = () => {
   const { t } = useTranslation('common');
 
+  const navigate = useNavigate();
+
   return (
     <LandingContainer>
       <Label size="title">{t('welcome')}</Label>
+      <Button onClick={() => navigate('/newClassroom')}>{t('addClassroom')}</Button>
     </LandingContainer>
   );
 };
