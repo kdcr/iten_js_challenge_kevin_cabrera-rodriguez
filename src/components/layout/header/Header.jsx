f2f8cea6019d9a4007955f1d326ea0f7a9cd9abb
kdcr <kevincabrerarodriguez@gmail.com>
2022-05-07 13:22:46 +0100

    Header component development Added header component to layout wrapper

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
+import { DeviceSizes } from '../../../utils/Constants';
+
 import Label from '../../atomic/label';
 
 const HeaderContainer = styled.div`
@@ -10,6 +12,14 @@ const HeaderContainer = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+
+  background-color: var(--color-accent);
+
+  height: 50px;
+  @media (min-width: ${DeviceSizes.md}) {
+    height: 90px;
+    padding: 0 25px 0 25px;
+  }
 `;
 
 const Header = () => {
@@ -21,7 +31,7 @@ const Header = () => {
         <Label size="subtitle">{t('appTitle')}</Label>
       </Link>
       <Link to="/">
-        <Label size="subtitle">{t('appTitle')}</Label>
+        <Label size="subtitle">{t('classrooms')}</Label>
       </Link>
     </HeaderContainer>
   );
