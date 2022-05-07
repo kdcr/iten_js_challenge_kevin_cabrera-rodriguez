cc5fd98f062fc117ffc88f52979b9c89a8248d86
kdcr <kevincabrerarodriguez@gmail.com>
2022-05-07 11:56:18 +0100

    Implemented basic i18n configuration Added common i18n namespace

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './components/layout/App';
 
 import './index.css';
+import './i18n';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
