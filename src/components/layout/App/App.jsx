e7e0286815ab25d1160933eebb817921686f78a3
kdcr <kevincabrerarodriguez@gmail.com>
2022-05-07 17:55:34 +0100

    Load mock-up data if couldn't find any

--- src/components/layout/App/App.jsx
+++ src/components/layout/App/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { AppThemes } from '../../../utils/Constants';
 import Landing from '../landing';
 import LayoutWrapper from '../layoutWrapper';
+import { writeData } from '../../../api/ApiUtils';
+import MockupData from '../../../api/MockupData.json';
 
 const App = (props) => {
   const { theme } = props;
+
+  useEffect(() => {
+    if (!localStorage.itenData) {
+      writeData(MockupData);
+    }
+  }, []);
 
   return (
     <div className={`${theme === AppThemes.LIGHT ? 'light-theme' : 'dark-theme'}`}>
